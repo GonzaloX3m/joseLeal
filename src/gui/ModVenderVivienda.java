98413fc7fc0136cbdab415b788a3704ed28847cb
Jose Leal <lealxh@gmail.com>
2018-05-29 18:03:16 -0400

    Actualizacion en Vender vivienda

--- src/gui/ModVenderVivienda.java
+++ src/gui/ModVenderVivienda.java
@@ -6,6 +6,7 @@ import java.sql.SQLException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -47,6 +48,8 @@ public class ModVenderVivienda extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        GrupoEstado = new javax.swing.ButtonGroup();
+        GrupoOrdenPrecio = new javax.swing.ButtonGroup();
         jPanel1 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
         btnVolver = new javax.swing.JButton();
@@ -85,8 +88,10 @@ public class ModVenderVivienda extends javax.swing.JFrame {
 
         jLabel7.setText("Estado  Vivienda:");
 
+        GrupoEstado.add(rbdUsada);
         rbdUsada.setText("Usada");
 
+        GrupoEstado.add(rbdNueva);
         rbdNueva.setSelected(true);
         rbdNueva.setText("Nueva");
 
@@ -112,16 +117,13 @@ public class ModVenderVivienda extends javax.swing.JFrame {
         ));
         tblViviendasVenta.setName("tblViviendas"); // NOI18N
         tblViviendasVenta.setSelectionMode(javax.swing.ListSelectionModel.SINGLE_SELECTION);
-        tblViviendasVenta.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                tblViviendasVentaMouseClicked(evt);
-            }
-        });
         jScrollPane2.setViewportView(tblViviendasVenta);
 
+        GrupoOrdenPrecio.add(precAsc);
         precAsc.setSelected(true);
         precAsc.setText("ASC");
 
+        GrupoOrdenPrecio.add(precDesc);
         precDesc.setText("DESC");
 
         cmbClientes.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
@@ -297,11 +299,6 @@ public class ModVenderVivienda extends javax.swing.JFrame {
         
     }//GEN-LAST:event_bntBuscarActionPerformed
 
-    private void tblViviendasVentaMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_tblViviendasVentaMouseClicked
-        // TODO add your handling code here:
-        
-    }//GEN-LAST:event_tblViviendasVentaMouseClicked
-
     private void cmbClientesActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cmbClientesActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_cmbClientesActionPerformed
@@ -311,24 +308,47 @@ public class ModVenderVivienda extends javax.swing.JFrame {
  
         if(viviendas!=null && !viviendas.isEmpty())
         {
-            Cliente c = (Cliente) cmbClientes.getSelectedItem();
-            
+           
             int row=tblViviendasVenta.getSelectedRow();
+            
             if(row>=0)
             {
-                Vivienda v= viviendas.get(row);
-                Usuario u=this.usuario;
-                
-                DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
-                LocalDateTime now = LocalDateTime.now();  
-                
-                Venta venta=new Venta();
-                venta.setCliente(c);
-                venta.setUsuario(u);
-                venta.setFecha(dateFormat.format(now));
+                Cliente c = (Cliente) cmbClientes.getSelectedItem();
+                if(c!=null)
+                {
+                        Vivienda v= viviendas.get(row);
+                        Usuario u=this.usuario;
+                        LocalDateTime now = LocalDateTime.now();  
+                        Venta venta=new Venta();
+                        venta.setCliente(c);
+                        venta.setUsuario(u);
+                        venta.setFecha(now.format(DateTimeFormatter.ISO_LOCAL_TIME));
+                }
+                else
+                {
+                        String titulo = "Advertencia!";
+                        int tipoMensaje = JOptionPane.WARNING_MESSAGE;
+                        JOptionPane.showMessageDialog(this, "Debe seleccionar el cliente al que le desea vender", titulo, tipoMensaje);
+                }
+               
+            }
+            else
+            {
+                String titulo = "Advertencia!";
+                int tipoMensaje = JOptionPane.WARNING_MESSAGE;
+                JOptionPane.showMessageDialog(this, "Debe seleccionar la vivienda que desea vender", titulo, tipoMensaje);
+
             }
             
             
+        
+        }
+        else
+        {
+                String titulo = "Advertencia!";
+                int tipoMensaje = JOptionPane.WARNING_MESSAGE;
+                JOptionPane.showMessageDialog(this, "Debe buscar la vivienda que desea vender", titulo, tipoMensaje);
+
         
         }
         
@@ -371,6 +391,8 @@ public class ModVenderVivienda extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.ButtonGroup GrupoEstado;
+    private javax.swing.ButtonGroup GrupoOrdenPrecio;
     private javax.swing.JButton bntBuscar;
     private javax.swing.JButton btnVender;
     private javax.swing.JButton btnVolver;
